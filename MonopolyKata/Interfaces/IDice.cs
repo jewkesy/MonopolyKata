76251fdb91e3174a3054f99ccda8ff062709ded1
jewkesy <jewkesy@a016fa64-353b-43a9-9919-22629269af4a>
2011-10-19 15:58:09 +0000

    started the coding, won't compile just yet...

---
+++ MonopolyKata/Interfaces/IDice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonopolyKata.Interfaces
+{
+    public interface IDice
+    {
+        int RollDice { get; set; }
+    }
+}
--- MonopolyKataTests/DieTests.cs
+++ MonopolyKataTests/DieTests.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace MonopolyKataTests
 {
     public class DieTests
     {
+        
         [Test]
         public void TestThatOnly2DieAreAllowed()
         {
@@ -23,7 +25,10 @@ namespace MonopolyKataTests
         [Test]
         public void TestThatTheHighstPossibleTotalScoreIs12()
         {
+            MonopolyKata.Interfaces.IDice mockedDice = MockRepository.GenerateMock<MonopolyKata.Interfaces.IDice>();
+            mockedDice.Expect(x => x.RollDice).Return(12);
 
+            mockedDice.VerifyAllExpectations();
         }
 
         [Test]
