b6d3cdfc854e3a825b297b9cf7e0b2119cfab320
jewkesy <jewkesy@a016fa64-353b-43a9-9919-22629269af4a>
2011-11-13 21:45:30 +0000

    additional unit test skeletons

--- MonopolyKata/Classes/Location/TitleDeed.cs
+++ MonopolyKata/Classes/Location/TitleDeed.cs
@@ -15,7 +15,8 @@ namespace MonopolyKata.Classes.Location
         public int ThreeHouses;
         public int FourHouses;
         public int Hotel;
-        public int CosttoBuild;
+        public int CostToBuild;
+        public int CostToSell { get { return CostToBuild/2; } }
         public int MortageValue {get { return Cost/2; }}
 
 
---
+++ MonopolyKata/Classes/Players/PlayerTokens.cs
@@ -0,0 +1,21 @@
+﻿namespace MonopolyKata.Classes.Players
+{
+    public class PlayerTokens
+    {
+        public enum Tokens
+        {
+            Battleship,
+            Hat,
+            Dog,
+            Car,
+            Iron,
+            Shoe,
+            Train,
+            Thimble,
+            Cannon,
+            Wheebarrow,
+            MoneyBag,
+            Horse
+        }
+    }
+}
